85b2b832053607518e0b3a2e70ec254e62eb26fe
Enzo Marin <enzzo.mariin@gmail.com>
2023-03-05 19:08:17 -0300

    conjuntos

---
+++ conjuntos.js
@@ -0,0 +1,16 @@
+/* 
+Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+- Un nuevo Set con los nombres de tu familia
+- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+ */
+
+const familia = ["padre", "madre", "hermano", "enzo"];
+const setFamilia = new Set(familia);
+console.log(setFamilia);
+
+setFamilia.add("enzo");
+console.log(setFamilia);
+
+setFamilia.add("javascript");
+console.log(setFamilia);
